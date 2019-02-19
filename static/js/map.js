0d398d92875c557e8cc7fa0b891e8d95d999460b
ecf <erinforman@gmail.com>
2019-02-19 09:04:18 -0800

    Add formatting to info window

--- static/js/map.js
+++ static/js/map.js
@@ -1,46 +1,66 @@
 "use strict";
 
 function initMap() {
-  
-$.get('/get_map_coords.json', (results) => {
-    let locations = results
 
-    const sfBayCoords = { lat: 37.601773, lng: -122.202870 };
+  let myCenter = { lat: 37.601773, lng: -122.202870 };
 
-    const map = new google.maps.Map(document.getElementById('map'), {
+  let map = new google.maps.Map(document.getElementById('map'), {
       zoom: 3,
-      center: sfBayCoords,
+      center: myCenter,
       //center: new google.maps.LatLng(41.976816, -87.659916),
-    });
+  });
+  
+  let infoWindow = new google.maps.InfoWindow({
+        width: 150
+  });
 
-    const infowindow = new google.maps.InfoWindow({});
+  //Retrieve user locations with AJAX
+  $.get('/get_map_coords.json', (results) => {
 
-    let marker, i;
+    let marker, position, html
+  
+    for (let location in results) {
 
-    for (i = 0; i < locations.length; i+=1) {
 
-      let position = new google.maps.LatLng(locations[i].lat, locations[i].lng)
       
-      marker = new google.maps.Marker({position: position,map: map});
+     marker = new google.maps.Marker({
+                          position: new google.maps.LatLng(results[location].lat, results[location].lng),
+                          map: map,
+                          animation: google.maps.Animation.DROP});
 
-      google.maps.event.addListener(marker, 'click', (function (marker, i) {
-        return function () {
-          infowindow.setContent(locations[i].url);
-          infowindow.open(map, marker);
-        }
-      }) (marker, i));
-      // function bindInfoWindow(marker, map, infoWindow, html) {
-      // google.maps.event.addListener(marker, 'click', (marker,i) => {
-      //       infoWindow.close();
-      //       infoWindow.setContent(locations[i].url);
-      //       infoWindow.open(map, marker);
-      //   });
+
+
+     
+      html = ('<div class="window-content">' +
+                    '<img src="/static/img/polarbear.jpg" alt="polarbear" style="width:150px;" class="thumbnail">' +
+                    '<p><b>Name: </b>' + '**Put the name here**'+ '</p>' +
+                    '<p><b>URL: </b>' + results[location].url + '</p>' +
+                    '<p><b>Recommended by: </b>' + results[location].recommended_by + '</p>' +
+                    '<p><b>Address: </b>' + results[location].formatted_address + '</p>' +
+                    '<p><b>Saved: </b>' + results[location].date_stamp + '</p>' +
+              '</div>');
+
+        bindInfoWindow(marker, map, infoWindow, html);
     }
-   });
+  });
+
+    function bindInfoWindow(marker, map, infoWindow, html) {
+        google.maps.event.addListener(marker, 'click', function () {
+            infoWindow.close();
+            infoWindow.setContent(html);
+            infoWindow.open(map, marker);
+        });
+    }
 }
 
 
 
+
+
+    
+
+
+ 
 
 
 
