409359bbf47d957a12caa728f2b13304bb3d984a
ecf <erinforman@gmail.com>
2019-03-09 19:20:24 -0800

    Declare browser for beautiful soup

--- beautiful_soup.py
+++ beautiful_soup.py
@@ -1,75 +1,79 @@
-from urllib.request import urlopen
+import requests
 from bs4 import BeautifulSoup
+# from urllib.request import urlopen
 
+headers = {'User-Agent':'Mozilla/5.0'}
 
 # url = "https://www.nytimes.com/2017/11/20/t-magazine/travel/cap-ferret-guide.html"
 
 def beautiful_soup(url):
 
     try:
-        bs_url_open = BeautifulSoup(urlopen(url),features="html.parser")
+        # bs_url_open = BeautifulSoup(urlopen(url),features="html.parser")
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, "html.parser")
     except:
         return
     else:
-        return bs_url_open
+        return soup
 
 ##############################################################################
 
-def search_url_image(url, beautiful_soup):
+def search_url_image(url, soup):
 
     try:
-        metatag_img = beautiful_soup.find("meta", {"property": "og:image"})
+        metatag_img = soup.find("meta", {"property": "og:image"})
         url_image = (metatag_img["content"]).strip()
     except:
         return
     else:
         return url_image
 
-def search_url_title(url, beautiful_soup):
+def search_url_title(url, soup):
 
     try:
-        metatag_title = beautiful_soup.find("meta", {"property": "og:title"})
+        metatag_title = soup.find("meta", {"property": "og:title"})
         url_title = (metatag_title["content"]).strip()
     except:
         return
     else:
         return url_title
 
-def search_url_head_title(url, beautiful_soup):
+def search_url_head_title(url, soup):
 
     try:
         """Result: <title> UKAHT - Visiting Port Lockroy</title> """
-        metatag_head_title = beautiful_soup.find("title")
+        metatag_head_title = soup.find("title")
         url_head_title = (metatag_head_title.get_text()).strip()
     except:
         return
     else:
         return url_head_title
 
-def search_url_author(url, beautiful_soup):
+def search_url_author(url, soup):
 
     try:
-        metatag_author = beautiful_soup.find("meta", {"property": "article:author"})
+        metatag_author = soup.find("meta", {"property": "article:author"})
         url_author = (metatag_author["content"]).strip()
     except:
         return
     else:
-        return metatag_author
+        return str(metatag_author)
 
-def search_url_site_name(url, beautiful_soup):
+def search_url_site_name(url, soup):
 
     try:
-        metatag_site_name = beautiful_soup.find("meta", {"property": "og:site_name"})
+        metatag_site_name = soup.find("meta", {"property": "og:site_name"})
         url_site_name = (metatag_site_name["content"]).strip()
     except:
         return
     else:
         return url_site_name
 
-def search_url_twitter(url, beautiful_soup):
+def search_url_twitter(url, soup):
 
     try:
-        metatag_twitter = beautiful_soup.find("meta", {"name": "twitter:domain"})
+        metatag_twitter = soup.find("meta", {"name": "twitter:domain"})
         url_twitter = (metatag_twitter["content"]).strip()
     except:
         return
